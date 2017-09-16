175b252141269d4f10bea561f336d4c154f7b11b
AkhilaReddyBekkem <31959305+AkhilaReddyBekkem@users.noreply.github.com>
2017-09-16 12:25:53 +0530

    my files
    exam of epam

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int n;
+Scanner s=new Scanner(System.in);
+		int Student[] students=new Student[n];
+
+n=s.nextInt();
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		Student s=new Student(523,Akhila Reddy,20-12-1996,500);
+		StudentGroup sg=new StudentGroup();
+		sg.getStudent(Student[] student);
+	}
+
+}
